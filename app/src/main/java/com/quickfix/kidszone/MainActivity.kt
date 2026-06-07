3711d63b0a0d1e3aca4f1044d0785848d1481cb2
AnuragKataria <katanu09@gmail,com>
2026-06-07 15:32:34 +0530

    Minor changes

--- app/src/main/java/com/quickfix/kidszone/MainActivity.kt
+++ app/src/main/java/com/quickfix/kidszone/MainActivity.kt
@@ -2,6 +2,7 @@ package com.quickfix.kidszone
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,7 +19,16 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                scrim = android.graphics.Color.TRANSPARENT,
+                darkScrim = android.graphics.Color.TRANSPARENT,
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                scrim = android.graphics.Color.TRANSPARENT,
+                darkScrim = android.graphics.Color.TRANSPARENT,
+            ),
+        )
         setContent {
             KidsZoneTheme {
                 Surface(modifier = Modifier.fillMaxSize().safeDrawingPadding()) {
